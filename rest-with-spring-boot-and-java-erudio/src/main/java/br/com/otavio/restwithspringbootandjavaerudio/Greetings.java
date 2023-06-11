7cc87efd0355f2d10c2e043ed8a6863bc8e1fa00
heinrik <heinrik10@gmail.com>
2023-06-11 15:26:39 -0300

    feat: Primeiros passos curso restful-apis-do-0-a-nuvem-com-springboot-e-docker

---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/otavio/restwithspringbootandjavaerudio/Greetings.java
@@ -0,0 +1,15 @@
+package br.com.otavio.restwithspringbootandjavaerudio;
+
+import lombok.Data;
+
+@Data
+public class Greetings {
+    private final Long id;
+    private final String content;
+
+
+    public Greetings(Long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+}
---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/otavio/restwithspringbootandjavaerudio/controllers/GeetingsController.java
@@ -0,0 +1,20 @@
+package br.com.otavio.restwithspringbootandjavaerudio.controllers;
+
+import br.com.otavio.restwithspringbootandjavaerudio.Greetings;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController()
+public class GeetingsController {
+
+    private final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greetings")
+    public Greetings greetings(@RequestParam(value = "name", defaultValue = "World") String name){
+        return new Greetings(counter.incrementAndGet(), String.format(template, name));
+    }
+}
