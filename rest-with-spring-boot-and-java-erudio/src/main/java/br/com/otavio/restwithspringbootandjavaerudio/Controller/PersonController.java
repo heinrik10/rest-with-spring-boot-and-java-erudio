9cfb393b4ea8309c469d6f767d416e741739f668
heinrik <heinrik10@gmail.com>
2023-07-26 22:11:15 -0300

    feat: Person using Content Negotiation

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/otavio/restwithspringbootandjavaerudio/Controller/PersonController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/otavio/restwithspringbootandjavaerudio/Controller/PersonController.java
@@ -10,50 +10,50 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static br.com.otavio.restwithspringbootandjavaerudio.Util.MediaType.APPLICATION_YAML;
+
 @RestController
 @RequestMapping("/api/v1/person")
 public class PersonController {
 
-    private PersonService personService;
-
+    private final PersonService personService;
     public PersonController(PersonService personService) {
         this.personService = personService;
     }
 
     @GetMapping(value = "/{id}",
-            produces = MediaType.APPLICATION_JSON_VALUE)
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, APPLICATION_YAML})
     public ResponseEntity<PersonVOv1> findById(@PathVariable Long id) {
         return ResponseEntity.ok(personService.findById(id));
     }
 
     @GetMapping(value = "/gender",
-            produces = MediaType.APPLICATION_JSON_VALUE)
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, APPLICATION_YAML})
     public ResponseEntity<List<PersonVOv1>> findByGender(@RequestParam Gender gender) {
         return ResponseEntity.ok(personService.findByGender(gender));
     }
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, APPLICATION_YAML})
     public ResponseEntity<List<PersonModel>> findAll() {
         return ResponseEntity.ok(personService.findAll());
     }
 
     @PostMapping(
-            produces = MediaType.APPLICATION_JSON_VALUE,
-            consumes = MediaType.APPLICATION_JSON_VALUE
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, APPLICATION_YAML},
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, APPLICATION_YAML}
     )
     public ResponseEntity<PersonVOv1> createNewPerson(@RequestBody PersonVOv1 personVOv1) {
         return ResponseEntity.ok(personService.createNewPerson(personVOv1));
     }
 
     @PutMapping(value = "/{id}",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, APPLICATION_YAML},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, APPLICATION_YAML})
     public ResponseEntity<PersonVOv1> updatePerson(@PathVariable Long id, @RequestBody PersonVOv1 personVOv1) {
         return ResponseEntity.ok(personService.updatePerson(id, personVOv1));
     }
 
-    @DeleteMapping(value = "/{id}",
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> deletePerson(@PathVariable Long id) {
         personService.deletePerson(id);
         return ResponseEntity.noContent().build();
---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/otavio/restwithspringbootandjavaerudio/Util/MediaType.java
@@ -0,0 +1,6 @@
+package br.com.otavio.restwithspringbootandjavaerudio.Util;
+
+public class MediaType {
+    public static final String APPLICATION_YAML = "application/x-yaml";
+
+}
