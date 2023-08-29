eae514c81ac3b51cd7c7454228ec866336e25216
heinrik <heinrik10@gmail.com>
2023-08-28 22:31:07 -0300

    feat: CORS + TestContainers

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/otavio/restwithspringbootandjavaerudio/Config/WebConfig.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/otavio/restwithspringbootandjavaerudio/Config/WebConfig.java
@@ -1,10 +1,12 @@
 package br.com.otavio.restwithspringbootandjavaerudio.Config;
 
 import br.com.otavio.restwithspringbootandjavaerudio.SerializationConverter.YamlJackson2HttpMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -13,7 +15,8 @@ import java.util.List;
 public class WebConfig implements WebMvcConfigurer{
     private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
 
-
+    @Value("${cors.originPatterns:default}")
+    private String corsOriginPatterns = "";
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer contentNegotiationConfigurer) {
@@ -35,4 +38,12 @@ public class WebConfig implements WebMvcConfigurer{
         converters.add(new YamlJackson2HttpMessageConverter());
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        var allowedOrigins = corsOriginPatterns.split(",");
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins(allowedOrigins)
+                .allowCredentials(true);
+    }
 }
